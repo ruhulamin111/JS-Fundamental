9bb00be9462802ded1131587d9d25852b08494ca
ruhulamin111 <ruhuljuwel111@gmail.com>
2022-07-26 12:35:37 +0600

    first commit

---
+++ fundamental.js
@@ -0,0 +1,30 @@
+// array 
+var footballTeam = ['Ronaldo', 'Messi', 'Neymar', 'Sadio Mane', 'Mohammad Saleh'];
+
+// array length
+var total = footballTeam.length;
+
+// Array type
+var category = typeof (footballTeam);
+
+// Array index 
+var preface = footballTeam.indexOf('Ronaldo')
+
+// Array position 
+var position = footballTeam[3];
+
+// Array add element 
+var add = footballTeam.push('Jidney');
+
+// array remove element 
+var remove = footballTeam.pop();
+
+// array index add element 
+footballTeam[2] = 'Abir Chowdhury'
+
+// array add first element 
+footballTeam.unshift('Ronal Dinho');
+
+// array remove first element 
+footballTeam.shift();
+
